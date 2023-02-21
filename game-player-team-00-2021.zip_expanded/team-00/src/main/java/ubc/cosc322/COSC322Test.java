db55a9462717b7804374cba9667f21a312014e55
Ben Keeley <bkeeley.50@gmail.com>
2023-02-20 23:31:05 -0800

    Added a simple check opponent's moves. May not cover all edge cases.

--- game-player-team-00-2021.zip_expanded/team-00/src/main/java/ubc/cosc322/COSC322Test.java
+++ game-player-team-00-2021.zip_expanded/team-00/src/main/java/ubc/cosc322/COSC322Test.java
@@ -15,111 +15,243 @@ import ygraph.ai.smartfox.games.amazons.HumanPlayer;
 
 /**
  * An example illustrating how to implement a GamePlayer
- * @author Yong Gao (yong.gao@ubc.ca)
- * Jan 5, 2021
+ * 
+ * @author Yong Gao (yong.gao@ubc.ca) Jan 5, 2021
  *
  */
-public class COSC322Test extends GamePlayer{
+public class COSC322Test extends GamePlayer {
 
-    private GameClient gameClient = null; 
-    private BaseGameGUI gamegui = null;
+	private GameClient gameClient = null;
+	private BaseGameGUI gamegui = null;
+
+	private String userName = null;
+	private String passwd = null;
+
+	int[][] board;
+	private int whiteQueen = 1;
+	private int blackQueen = 2;
+	private int arrow = 3; // could be any number other than 1, 2
+	private int myQueen = -1;
+	private int opponentQueen = -1;
+
+	/**
+	 * The main method
+	 * 
+	 * @param args for name and passwd (current, any string would work)
+	 */
+	public static void main(String[] args) {
+		COSC322Test player = new COSC322Test("user", "pass");
+		
+		//HumanPlayer player = new HumanPlayer();
 	
-    private String userName = null;
-    private String passwd = null;
-    
-    int[][] board;
-    private int whiteQueen = 1;
-    private int blackQueen = 2;
-    private int arrow = 3; //could be any number other than 1, 2
-    private int myQueen = -1;
-    private int opponentQueen = -1;
- 
-	
-    /**
-     * The main method
-     * @param args for name and passwd (current, any string would work)
-     */
-    public static void main(String[] args) {				 
-    	//COSC322Test player = new COSC322Test(args[0], args[1]);
-    	HumanPlayer player = new HumanPlayer();
-    	
-    	if(player.getGameGUI() == null) {
-    		player.Go();
-    	}
-    	else {
-    		BaseGameGUI.sys_setup();
-            java.awt.EventQueue.invokeLater(new Runnable() {
-                public void run() {
-                	player.Go();
-                }
-            });
-    	}
-    }
-	
-    /**
-     * Any name and passwd 
-     * @param userName
-      * @param passwd
-     */
-    public COSC322Test(String userName, String passwd) {
-    	this.userName = userName;
-    	this.passwd = passwd;
-    	this.board = new int[10][10];
-    	
-    	//To make a GUI-based player, create an instance of BaseGameGUI
-    	//and implement the method getGameGUI() accordingly
-    	this.gamegui = new BaseGameGUI(this);
-    }
- 
+		if (player.getGameGUI() == null) {
+			player.Go();
+		} else {
+			BaseGameGUI.sys_setup();
+			java.awt.EventQueue.invokeLater(new Runnable() {
+				public void run() {
+					player.Go();
+				}
+			});
+		}
+	}
 
+	/**
+	 * Any name and passwd
+	 * 
+	 * @param userName
+	 * @param passwd
+	 */
+	public COSC322Test(String userName, String passwd) {
+		this.userName = userName;
+		this.passwd = passwd;
+		this.board = new int[10][10];
+		
+		//hard coded but ideally set in handler.
+		this.board[0][6] = this.whiteQueen;
+		this.board[0][3] = this.whiteQueen;
+		
+		this.board[4][0] = this.whiteQueen;
+		this.board[4][9] = this.whiteQueen;
+		
+		this.board[9][3] = this.blackQueen;
+		this.board[9][6] = this.blackQueen;
+		
+		this.board[6][0] = this.blackQueen;
+		this.board[6][9] = this.blackQueen;
 
-    @Override
-    public void onLogin() {
-        userName = gameClient.getUserName(); 
-        if(gamegui != null) { 
-        gamegui.setRoomInformation(gameClient.getRoomList()); 
-        } 
-    }
+		// comment
+		// To make a GUI-based player, create an instance of BaseGameGUI
+		// and implement the method getGameGUI() accordingly
+		this.gamegui = new BaseGameGUI(this);
+	}
 
-    @SuppressWarnings("unchecked")
 	@Override
-    public boolean handleGameMessage(String messageType, Map<String, Object> msgDetails) {
-    	//This method will be called by the GameClient when it receives a game-related message
-    	//from the server.
+	public void onLogin() {
+		userName = gameClient.getUserName();
+		if (gamegui != null) {
+			gamegui.setRoomInformation(gameClient.getRoomList());
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean handleGameMessage(String messageType, Map<String, Object> msgDetails) {
+		// This method will be called by the GameClient when it receives a game-related
+		// message
+		// from the server.
+
+		// For a detailed description of the message types and format,
+		// see the method GamePlayer.handleGameMessage() in the game-client-api
+		// document.
+		switch (messageType) {
+		case GameMessage.GAME_STATE_BOARD:
+			ArrayList<Integer> stateArr = (ArrayList<Integer>) (msgDetails.get(AmazonsGameMessage.GAME_STATE));
+			this.getGameGUI().setGameState(stateArr);
+			break; /* THIS GAME STATE BOARD IS A MESSAGE CONTAINING THE CURRENT STATE */
+		case GameMessage.GAME_ACTION_MOVE:
+			/// CHECK IF MOVE IS LEGAL HERE!!!!!!!!!!						
+			ArrayList<Integer> queenPosCurr = (ArrayList<Integer>) (msgDetails.get(AmazonsGameMessage.QUEEN_POS_CURR));
+			ArrayList<Integer> queenPosNext = (ArrayList<Integer>) (msgDetails.get(AmazonsGameMessage.QUEEN_POS_NEXT));
+			ArrayList<Integer> arrowPos = (ArrayList<Integer>) (msgDetails.get(AmazonsGameMessage.ARROW_POS));
+			
+			int queenPosCurrRow = queenPosCurr.get(0);
+			int queenPosCurrColumn = queenPosCurr.get(1);
+			
+			int queenPosNextRow = queenPosNext.get(0);
+			int queenPosNextColumn = queenPosNext.get(1);
+			
+			int arrowPosRow = arrowPos.get(0);
+			int arrowPosColumn = arrowPos.get(1);
+			
+			int queenPosCurrValue = this.board[queenPosCurrRow-1][queenPosCurrColumn-1];
+			int queenPosNextValue = this.board[queenPosNextRow-1][queenPosNextColumn-1];
+			
+			//int arrowPosValue = this.board[arrowPosRow-1][arrowPosColumn-1];
+					
+			// if it is legal, update our local board and the gui, then let the AI run
+			// if it is ilegal, report the illegal move!
+			
+			boolean legal = CheckMoveHelper(queenPosCurrRow, queenPosCurrColumn, queenPosNextRow, queenPosNextColumn);
+			
+			this.board[queenPosCurr.get(0)-1][queenPosCurr.get(1)-1] = 0;
+			this.board[queenPosNext.get(0)-1][queenPosNext.get(1)-1] = this.opponentQueen;
+			
+			legal = legal&&CheckMoveHelper(queenPosNextRow, queenPosNextColumn, arrowPosRow, arrowPosColumn);
+
+			System.out.println("Legal " + legal);
+			
+			if(legal) {
+				this.board[arrowPos.get(0)-1][arrowPos.get(1)-1] = this.arrow;
+				this.getGameGUI().updateGameState(msgDetails);
+			}else {
+				this.board[queenPosCurr.get(0)-1][queenPosCurr.get(1)-1] = queenPosCurrValue;
+				this.board[queenPosNext.get(0)-1][queenPosNext.get(1)-1] = queenPosNextValue;
+				assert(false);
+			}
+
+			break;
+		case GameMessage.GAME_ACTION_START:
+			String playingWhiteQueens = (String) msgDetails.get(AmazonsGameMessage.PLAYER_WHITE);
+			String playingBlackQueens = (String) msgDetails.get(AmazonsGameMessage.PLAYER_BLACK);
+			assert (!playingWhiteQueens.equals(playingBlackQueens));
+		default:
+			assert (false);
+			break;
+		}
+		return true;
+	}
 	
-    	//For a detailed description of the message types and format, 
-    	//see the method GamePlayer.handleGameMessage() in the game-client-api document. 
-    	switch(messageType) {
-    	case GameMessage.GAME_STATE_BOARD:
-    		ArrayList<Integer> stateArr = (ArrayList<Integer>)(msgDetails.get(AmazonsGameMessage.GAME_STATE));
-    		this.getGameGUI().setGameState(stateArr);
-    		break; /*  THIS GAME STATE BOARD IS A MESSAGE CONTAINING THE CURRENT STATE*/
-    	case GameMessage.GAME_ACTION_MOVE:
-    		///CHECK IF MOVE IS LEGAL HERE!!!!!!!!!!
-    		//if it is legal, update our local board and the gui, then let the AI run
-    		//if it is ilegal, report the illegal move!
-    		this.getGameGUI().updateGameState(msgDetails);
-    	case GameMessage.GAME_ACTION_START:
-    		String playingWhiteQueens = (String)msgDetails.get(AmazonsGameMessage.PLAYER_WHITE);
-    		String playingBlackQueens = (String)msgDetails.get(AmazonsGameMessage.PLAYER_BLACK);
-    		assert(!playingWhiteQueens.equals(playingBlackQueens));
-    	default:
-    		assert(false);
-    		break;
+	public boolean MoveIsLegal2(ArrayList<Integer> posCurr, ArrayList<Integer> posNext) {
+		
+		int posCurrRow = posCurr.get(0);
+		int posCurrColumn = posCurr.get(1);
+		
+		int posNextRow = posNext.get(0);
+		int posNextColumn = posNext.get(1);
+		
+		return CheckMoveHelper(posCurrRow, posCurrColumn, posNextRow, posNextColumn);
+	}
+		
+	public boolean CheckMoveHelper(int startRow, int startColumn, int targetRow, int targetColumn) {
+		
+		//must move
+		if(startRow == targetRow && startColumn == targetColumn)
+			return false;
+		
+		int rowDirection = targetRow - startRow > 0 ? 1: -1;
+		int columnDirection = targetColumn - startColumn > 0 ? 1: -1;
+		
+		if(startRow == targetRow) {
+			//move horizontally
+			return CheckMove(startRow, startColumn, targetRow, targetColumn, 0, columnDirection);
+		}else if(startColumn == targetColumn) {
+			//move vertical
+			return CheckMove(startRow, startColumn, targetRow, targetColumn, rowDirection, 0);
+		}else {
+			//check diagonal
+			return CheckMove(startRow, startColumn, targetRow, targetColumn, rowDirection, columnDirection);
+		}
+	}
+	
+	
+	public boolean CheckMove(int startRow, int startColumn, int targetRow, int targetColumn, int rowDirection, int columnDirection) {
+		
+		//start at 1 position away because the player has to move.
+		int posRow = startRow + rowDirection;
+		int posColumn = startColumn + columnDirection;
+		
+		while(OnGameBoard(posRow, posColumn)) {
+			
+			int val = this.board[posRow-1][posColumn-1];
+			
+			if(val != 0) {
+				return false;
+			}
+			else if(posRow == targetRow && posColumn == targetColumn) {
+				return true;
+			}
+				
+			posRow += rowDirection;	
+			posRow += columnDirection;
+		}
+		
+		return false;
+	}
+	
+		
+	public boolean OnGameBoard (int row, int column) {
+		return ValidRowOrColumn(row) && ValidRowOrColumn(column);
+	}
+	
+	public boolean ValidRowOrColumn(int rowOrColumnPos) { 
+		return rowOrColumnPos > 0 && rowOrColumnPos <=10;
+	}
+
+	// WRITE A METHOD THAT SETS MYQUEEN TO 1 IF THIS.USERNAME = PLAYINGWHITEQUEENS,
+	// and THE OPPOSITE IF THIS.USERNAME = PLAYINGBLACKQUEENS
+	public void SetMyQueen() {
+    	if(this.userName().equals("PLAYINGWHITEQUEENS")) {
+    		 this.myQueen = 1;
+    		 this.opponentQueen = 2;
+    	}else if (this.userName().equals("PLAYINGBLACKQUEENS") ) {
+    		  this.myQueen = 2;
+    		  this.opponentQueen = 1;
     	}
-    	return true;   	
     }
-    //WRITE A METHOD THAT SETS MYQUEEN TO 1 IF THIS.USERNAME = PLAYINGWHITEQUEENS, and THE OPPOSITE IF THIS.USERNAME = PLAYINGBLACKQUEENS
-    
-    ///WRITE A METHOD INITIALIZE GAME BOARD THAT TAKES THE ARRAYLIST FROM THE SERVER (121 indexes) AND CONVERTS IT TO THE BOARD[][] ARRAY
-    
-    //WRITE A METHOD THAT DISPLAYS THE CURRENT STATE OF THE BOARD AS TEXT, FOR TESTING PURPOSES, ALSO WRITE THESE MOVES TO A TEXT
-    //FILE SO THAT YOU HAVE PROOF THAT THE OPPONENT MADE AN ILLEGAL MOVE AFTER THE GAME
-    
-    @Override
-    public String userName() {
-    	return userName;
-    }
+
+	/// WRITE A METHOD INITIALIZE GAME BOARD THAT TAKES THE ARRAYLIST FROM THE
+	/// SERVER (121 indexes) AND CONVERTS IT TO THE BOARD[][] ARRAY
+
+	// WRITE A METHOD THAT DISPLAYS THE CURRENT STATE OF THE BOARD AS TEXT, FOR
+	// TESTING PURPOSES, ALSO WRITE THESE MOVES TO A TEXT
+	// FILE SO THAT YOU HAVE PROOF THAT THE OPPONENT MADE AN ILLEGAL MOVE AFTER THE
+	// GAME
+
+	@Override
+	public String userName() {
+		return userName;
+	}
 
 	@Override
 	public GameClient getGameClient() {
@@ -130,14 +262,13 @@ public class COSC322Test extends GamePlayer{
 	@Override
 	public BaseGameGUI getGameGUI() {
 		// TODO Auto-generated method stub
-		return  this.gamegui;
+		return this.gamegui;
 	}
 
 	@Override
 	public void connect() {
 		// TODO Auto-generated method stub
-    	gameClient = new GameClient(userName, passwd, this);			
+		gameClient = new GameClient(userName, passwd, this);
 	}
 
- 
-}//end of class
+}// end of class
