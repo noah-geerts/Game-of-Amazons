ad5c080ace8fae1cc5118f74ee561cfce2c03ddf
Ben Keeley <bkeeley.50@gmail.com>
2023-03-07 18:07:13 -0800

    Set up the AI to play against itself.

--- game-player-team-00-2021.zip_expanded/team-00/src/main/java/ubc/cosc322/COSC322Test.java
+++ game-player-team-00-2021.zip_expanded/team-00/src/main/java/ubc/cosc322/COSC322Test.java
@@ -33,6 +33,8 @@ public class COSC322Test extends GamePlayer {
 	private int arrow = 3; // could be any number other than 1, 2
 	private int myQueen = -1;
 	private int opponentQueen = -1;
+	
+	private boolean movedFirst = true;
 
 	/**
 	 * The main method
@@ -40,7 +42,7 @@ public class COSC322Test extends GamePlayer {
 	 * @param args for name and passwd (current, any string would work)
 	 */
 	public static void main(String[] args) {
-		COSC322Test player = new COSC322Test("user1", "pass");
+		COSC322Test player = new COSC322Test("user2", "pass");
 		//HumanPlayer player = new HumanPlayer();
 
 		if (player.getGameGUI() == null) {
@@ -65,20 +67,7 @@ public class COSC322Test extends GamePlayer {
 		this.userName = userName;
 		this.passwd = passwd;
 		
-		this.board = new int[10][10];
-
-		// hard coded but ideally set using stateArr
-		this.board[0][3] = this.whiteQueen;
-		this.board[0][6] = this.whiteQueen;
-
-		this.board[3][0] = this.whiteQueen;
-		this.board[3][9] = this.whiteQueen;
-
-		this.board[6][0] = this.blackQueen;
-		this.board[6][9] = this.blackQueen;
-
-		this.board[9][3] = this.blackQueen;
-		this.board[9][6] = this.blackQueen;
+		InitalizeBoard();
 
 		// To make a GUI-based player, create an instance of BaseGameGUI
 		// and implement the method getGameGUI() accordingly
@@ -104,54 +93,37 @@ public class COSC322Test extends GamePlayer {
 		// see the method GamePlayer.handleGameMessage() in the game-client-api
 		// document.
 		switch (messageType) {
-		case GameMessage.GAME_STATE_BOARD:
-			
-			System.out.println("GameBoard Start");
-						
+		case GameMessage.GAME_STATE_BOARD:					
 			ArrayList<Integer> stateArr = (ArrayList<Integer>) (msgDetails.get(AmazonsGameMessage.GAME_STATE));
 			this.getGameGUI().setGameState(stateArr);
+			
+			System.out.println("Board Start");
+			
 			break; /* THIS GAME STATE BOARD IS A MESSAGE CONTAINING THE CURRENT STATE */
 		case GameMessage.GAME_ACTION_MOVE:
-
+			
+			System.out.println("Move");
+			
 			ArrayList<Integer> queenPosCurr = (ArrayList<Integer>) (msgDetails.get(AmazonsGameMessage.QUEEN_POS_CURR));
 			ArrayList<Integer> queenPosNext = (ArrayList<Integer>) (msgDetails.get(AmazonsGameMessage.QUEEN_POS_NEXT));
 			ArrayList<Integer> arrowPos = (ArrayList<Integer>) (msgDetails.get(AmazonsGameMessage.ARROW_POS));
-			
+						
 			this.SetBoardState(queenPosCurr, queenPosNext, arrowPos, this.opponentQueen);
 			this.getGameGUI().updateGameState(msgDetails);
+			
+			MakeMove();
 
-			int[][][] state = new int[2][10][10];
-
-			state[0] = this.board;
-			state[1] = AmazonsUtility.getMobilityMap(this.board);
-			
-			MonteCarlo ai = new MonteCarlo(new TreeNode(state, this.myQueen), 100, 3);
-			AmazonsAction a = ai.MCTS();
-
-			ArrayList<Integer> aiQueenPosCurr = new ArrayList<Integer>();
-			aiQueenPosCurr.add(a.queenSrcY + 1);
-			aiQueenPosCurr.add(a.queenSrcX + 1);
-			
-			ArrayList<Integer> aiQueenPosNext = new ArrayList<Integer>();
-			aiQueenPosNext.add(a.queenDestY + 1);
-			aiQueenPosNext.add(a.queenDestX + 1);
-			
-			ArrayList<Integer> aiArrowPos = new ArrayList<Integer>();
-			aiArrowPos.add(a.arrowDestY + 1);
-			aiArrowPos.add(a.arrowDestX + 1);
-			
-			this.SetBoardState(aiQueenPosCurr, aiQueenPosNext, aiArrowPos, this.myQueen);
-			
-			this.getGameGUI().updateGameState(aiQueenPosCurr, aiQueenPosNext, aiArrowPos);
-			this.getGameClient().sendMoveMessage(aiQueenPosCurr, aiQueenPosNext, aiArrowPos);
-		
-			
 		case GameMessage.GAME_ACTION_START:
 			
 			String playingWhiteQueens = (String) msgDetails.get(AmazonsGameMessage.PLAYER_WHITE);
 			String playingBlackQueens = (String) msgDetails.get(AmazonsGameMessage.PLAYER_BLACK);
 			assert (!playingWhiteQueens.equals(playingBlackQueens));	
 			SetMyQueen(playingWhiteQueens, playingBlackQueens);
+			
+			if(this.myQueen == 1 && movedFirst) {
+				movedFirst = false;
+				MakeMove();
+			}
 			
 		default:
 			assert (false);
@@ -182,11 +154,61 @@ public class COSC322Test extends GamePlayer {
 	// FILE SO THAT YOU HAVE PROOF THAT THE OPPONENT MADE AN ILLEGAL MOVE AFTER THE
 	// GAME
 	
+	public void MakeMove() {
+		int[][][] state = new int[2][10][10];
+
+		state[0] = this.board;
+		state[1] = AmazonsUtility.getMobilityMap(this.board);
+		
+		MonteCarlo ai = new MonteCarlo(new TreeNode(state, this.myQueen), 100, 3);
+		AmazonsAction a = ai.MCTS();
+		
+		if(a!=null) {
+		
+		ArrayList<Integer> aiQueenPosCurr = new ArrayList<Integer>();
+		aiQueenPosCurr.add(a.queenSrcY + 1);
+		aiQueenPosCurr.add(a.queenSrcX + 1);
+		
+		ArrayList<Integer> aiQueenPosNext = new ArrayList<Integer>();
+		aiQueenPosNext.add(a.queenDestY + 1);
+		aiQueenPosNext.add(a.queenDestX + 1);
+		
+		ArrayList<Integer> aiArrowPos = new ArrayList<Integer>();
+		aiArrowPos.add(a.arrowDestY + 1);
+		aiArrowPos.add(a.arrowDestX + 1);
+		
+		this.SetBoardState(aiQueenPosCurr, aiQueenPosNext, aiArrowPos, this.myQueen);
+		
+		this.getGameGUI().updateGameState(aiQueenPosCurr, aiQueenPosNext, aiArrowPos);
+		this.getGameClient().sendMoveMessage(aiQueenPosCurr, aiQueenPosNext, aiArrowPos);
+		}
+	}
+	
 	public void SetBoardState(ArrayList<Integer> queenPosCurrent, ArrayList<Integer> queenPosNew, ArrayList<Integer> arrowPos, int queen) {
 		// update the board state.
 		this.board[queenPosCurrent.get(0) - 1][queenPosCurrent.get(1) - 1] = 0;
 		this.board[queenPosNew.get(0) - 1][queenPosNew.get(1) - 1] = queen;
 		this.board[arrowPos.get(0) - 1][arrowPos.get(1) - 1] = this.arrow;
+	}
+	
+	public void InitalizeBoard() {
+		
+		movedFirst = true;
+		
+		this.board = new int[10][10];
+
+		// hard coded but ideally set using stateArr
+		this.board[0][3] = this.whiteQueen;
+		this.board[0][6] = this.whiteQueen;
+
+		this.board[3][0] = this.whiteQueen;
+		this.board[3][9] = this.whiteQueen;
+
+		this.board[6][0] = this.blackQueen;
+		this.board[6][9] = this.blackQueen;
+
+		this.board[9][3] = this.blackQueen;
+		this.board[9][6] = this.blackQueen;
 	}
 
 	@Override
